c908ded456bedcc765f57b616ffac9736a5b2d1a
PK-Yakkun <mokkun515@gmail.com>
2022-06-16 23:25:50 +0900

    feat: TodoList作成

--- electron/renderer/pages/index.tsx
+++ electron/renderer/pages/index.tsx
@@ -1,34 +1,57 @@
-import { useEffect } from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const IndexPage = () => {
+  const [value, setValue] = useState<string>("");
+  const [listItem, setListItem] = useState<string[]>([]);
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    value && setListItem([...listItem, value]);
+    setValue("");
+  };
+  const onPressDeleteButton = () => {
+    setListItem([]);
+  };
   useEffect(() => {
-    const handleMessage = (_event, args) => alert(args)
+    const handleMessage = (_event, args) => alert(args);
 
     // add a listener to 'message' channel
-    global.ipcRenderer.addListener('message', handleMessage)
+    global.ipcRenderer.addListener("message", handleMessage);
 
     return () => {
-      global.ipcRenderer.removeListener('message', handleMessage)
-    }
-  }, [])
+      global.ipcRenderer.removeListener("message", handleMessage);
+    };
+  }, []);
 
   const onSayHiClick = () => {
-    global.ipcRenderer.send('message', 'hi from next')
-  }
+    global.ipcRenderer.send("message", "hi from next");
+  };
 
   return (
-    <Layout title="Home | Next.js + TypeScript + Electron Example">
-      <h1>Hello Next.js 👋</h1>
-      <button onClick={onSayHiClick}>Say hi to electron</button>
-      <p>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-      </p>
-    </Layout>
-  )
-}
+    <>
+      <ul>
+        {listItem.map((item, index) => (
+          <div key={index} style={{ display: "flex", alignItems: "center" }}>
+            <input type="checkbox" id={`${index}`} />
+            <label htmlFor={`${index}`}>
+              <li style={{ listStyle: "none" }}>{item}</li>
+            </label>
+          </div>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={value}
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit">追加</button>
+      </form>
+      <button onClick={() => onPressDeleteButton()}>すべて破棄する</button>
+    </>
+  );
+};
 
-export default IndexPage
+export default IndexPage;
